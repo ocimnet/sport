ffdf20b21b2baeecd86f3d33f440a10367954a5c
Ali Munandar <alimunandar@gmail.com>
2025-12-11 20:05:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">OcimScripts</div>
+                <div class="title">OcimScripts php</div>
             </div>
         </div>
     </body>
